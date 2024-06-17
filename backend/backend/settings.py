801f3c4b3dc824e58c273a21e066f6c2679f747f
Juanabely <abelkipkorir185@gmail.com>
2024-06-17 18:16:34 +0300

    Updated settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-7^&uiiq1pi@c889$7%lx!_h=-5v##tx6pps*m)yu4=2mh9d=4w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app",'127.0.0.1']
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
