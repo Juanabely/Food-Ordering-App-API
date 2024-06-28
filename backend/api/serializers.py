59edd426d8e2b16437c7e813ac9b7d0dc70730e1
Juanabely <abelkipkorir185@gmail.com>
2024-06-28 18:51:05 +0300

    update v.1.02

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -10,14 +10,38 @@ class UserSerializer(serializers.ModelSerializer):
     def create (self,validated_data):
          user = CustomUser.objects.create_user(**validated_data) 
          return user  
-    
+
+from rest_framework import serializers
+from.models import Food, Orders
+
+class FoodItemSerializer(serializers.Serializer):
+    food_id = serializers.PrimaryKeyRelatedField(queryset=Food.objects.all())
+    quantity = serializers.IntegerField()
+    price = serializers.IntegerField()
+
+    def validate(self, data):
+        # Ensure the calculated price matches the expected price
+        food_item = data['food_id']
+        expected_price = food_item.price * data['quantity']
+        if data['price']!= expected_price:
+            raise serializers.ValidationError({"price": "Calculated price does not match expected price."})
+        return data
+
 class FoodSerializer (serializers.ModelSerializer):
     class Meta:
         model = Food
         fields  = '__all__'
 
-class OrderSerializer (serializers.ModelSerializer):
-    class Meta :
+class OrderSerializer(serializers.ModelSerializer):
+    food_items = FoodItemSerializer(many=True)
+
+    class Meta:
         model = Orders
-        fields = '__all__'
-        extra_kwargs = {"user":{"write_only":True}}
+        fields = ['user', 'food_items']
+
+    def create(self, validated_data):
+        food_items_data = validated_data.pop('food_items')
+        order = Orders.objects.create(**validated_data)
+        for food_item_data in food_items_data:
+            FoodItemSerializer().create(food_item_data)
+        return order
